59ed36be641708e779d3944aa501f8cc88b5e26d
Dennis Kobert <dennis@kobert.dev>
2023-03-11 11:02:24 +0100

    Migrate webpack to parcel

--- frontend/src/state-providers/workspace.ts
+++ frontend/src/state-providers/workspace.ts
@@ -3,8 +3,8 @@
 import {tick} from "svelte";
 import {writable} from "svelte/store";
 
-import { type Editor } from "@graphite/wasm-communication/editor";
-import { UpdateNodeGraphVisibility } from "@graphite/wasm-communication/messages";
+import { type Editor } from "~/src/wasm-communication/editor";
+import { UpdateNodeGraphVisibility } from "~/src/wasm-communication/messages";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export function createWorkspaceState(editor: Editor) {
